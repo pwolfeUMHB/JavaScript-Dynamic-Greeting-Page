cc19ac4ce93ad0402d24e6471340ebbd12dcc520
Chance Thomas <cbthomas@mail.umhb.edu>
2024-02-20 11:24:43 -0600

    DGP

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('displayInput').addEventListener("click",fucntion(){
+
+    const userInput = document.getElementById('userInput').value;
+document.getElementById('output').textContent = userInput;
+
+
+});
